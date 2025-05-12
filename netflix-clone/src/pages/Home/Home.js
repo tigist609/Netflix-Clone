0a685d7c0f0d38227dd229d41718b562be6d5373
tigist609 <tegie88@yahoo.com>
2025-05-12 18:30:18 -0500

    Banner

--- netflix-clone/src/pages/Home/Home.js
+++ netflix-clone/src/pages/Home/Home.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import Header from '../../Components/Header/Header';
+import Banner from '../../Components/Banner/Banner';
 import Footer from '../../Components/Footer/Footer';
-
 const Home = () => {
   return (
     <>
 <Header/>
+<Banner/>
 <Footer/>
     </>
   )
